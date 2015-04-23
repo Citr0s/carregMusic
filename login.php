48b39bf2a84a5a120f8933512b85f55921514f2c
Milosz Dura <milosz.md@wp.pl>
2015-04-23 11:32:56 +0100

    testing login

--- login.php
+++ login.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Carreg Music</title>
-    <link rel="stylesheet" href="css/normalize.css" type="text/css">
-    <link rel="stylesheet" href="css/styles.css" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
-</head>
-<body>
-    
-    <div id="wrapper">
-        <div id="header">
-           <div class="container">
-               <div class="floatLeft">
-                    <a href="index.html"><img src="css/images/logo.png" alt="logo"></a>
-                </div>
-                <div class="floatLeft">
-                    <ul>
-                        <li><a href="index.html">HOME</a></li>
-                        <li><a href="">TRACKS</a></li>
-                        <li><a href="">ARTISTS</a></li>
-                        <li><a href="">CONCERTS</a></li>
-                    </ul>
-                </div>
-                <div class="floatRight">
-                    <ul>
-                        <li><a href="login.php">LOGIN</a></li>
-                        <li><a href="register.php" class="register">REGISTER</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div><!--end of head div-->
-        
+<?php include_once 'includes/header.php'; ?>
         <div class="container">
             <div class="formContainer">
                 <h2 class="bigH2">Log in</h2>
@@ -52,21 +19,4 @@
                 
             </div>
         </div>
-        
-        <div id="footer">
-            <div class="container">
-               <div class="floatLeft">
-                    <a href="index.html"><img src="css/images/logoBottom.png" alt="logo"></a>
-                </div>
-                <div class="floatLeft">
-                    <p>&copy; CARREG MUSIC 2015</p>
-                </div>
-            </div>
-        </div>
-        
-    </div><!--end of wrapper-->
-    
-    
-    <script src="js/scripts.js"></script>
-</body>
-</html>
+<?php include_once 'includes/footer.php'; ?>
