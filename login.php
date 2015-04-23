1aeecab088e29b7248985909bde847f7fbd341e0
Milosz Dura <milosz.md@wp.pl>
2015-04-23 11:31:29 +0100

    testing login

--- login.php
+++ login.php
@@ -59,7 +59,7 @@
                     <a href="index.html"><img src="css/images/logoBottom.png" alt="logo"></a>
                 </div>
                 <div class="floatLeft">
-                    <p>&copy; CARREG MUSIC 2015</p>
+                    <p>&copy; CARREG MUSIC 2015x</p>
                 </div>
             </div>
         </div>
