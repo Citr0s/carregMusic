818f548035b9a89bf917aa223c4ff428db26038b
Milosz Dura <milosz.md@wp.pl>
2015-04-24 22:05:23 +0100

    adding php login function and logout script

---
+++ functions/login.functions.php
@@ -0,0 +1,57 @@
+<?php
+function sanitise($value) {
+    return htmlentities($value);
+}
+
+function isRequired($needle, $haystack) {
+    return in_array($needle, $haystack);
+}
+
+function validStringMaxLength($value, $max) {
+    $isOkay = false;
+    if(strlen($value) <= $max && !is_numeric($value)) {
+        $isOkay = true;
+    }
+    return $isOkay;
+}
+
+function HtmlErrorMessage($field, $errorMessages) {
+    if(isset($errorMessages[$field])) {
+        echo '<span class="error_msg">' . sanitise($errorMessages[$field]) . '</span>';
+    }
+}
+
+function HtmlText($value) {
+    if(!empty($value)) {
+        echo sanitise($value);
+    }
+}
+
+function validateFormField($value, $field) {
+    $errorMessage = '';
+
+    switch($field) {
+        case 'username':
+            if(!validStringMaxLength($value, 25)) {
+                $errorMessage = 'Name must be 5 characters or less and must be a string';
+            }
+            break;
+        case 'password':
+            if(!validStringMaxLength($value, 25)) {
+                $errorMessage = 'Age must be a number between 18 and 75';
+            }
+            break;
+        default:
+    }
+    return $errorMessage;
+}
+
+function loggedIn(){
+    $loggedIn = false;
+
+    if(isset($_SESSION['username']) || isset($_COOKIE['username'])){
+        $loggedIn = true;
+    }
+
+    return $loggedIn;
+}
