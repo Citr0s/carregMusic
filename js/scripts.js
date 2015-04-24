fe3d3bec19372fd4cf7a6ea22b2dfa8e42b6366b
rogerogerson <addk92@gmail.com>
2015-04-24 09:15:04 +0100

    didnt commit properly re commited

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var errorArray = [];
 var userName = document.getElementById('userName');
+var email = document.getElementById('Email');
+var canSubmit = false;
 
 function addListener (object, event, functio) 
 {
@@ -11,23 +13,65 @@ function addListener (object, event, functio)
 }
 
 addListener(registerUserName, "blur", userNameVal);
+addListener(email, "blur", emailVal);
+addListener(Password1, "blur", passwordVal);
+addListener(PasswordCheck, "blur", passwordVal);
 
 function userNameVal () {
     
     //if (!isHTML5Supported()){
-            var isNameValid = false;
+       
         if(registerUserName.value.length == 0)
         {
-            isNameValid = false;
+            canSubmit = false;
             registerUserName.setAttribute("class", "notValid");
         }
         else { 
             registerUserName.removeAttribute("class", "notValid");
             registerUserName.setAttribute("class", "isValid");
-            isNameValid = true; 
+            canSubmit = true;
         }
             
             return isNameValid;
         
     //}//end of html5support
 }//end of valFirstName function
+
+function emailVal () {
+ //if(!isHTML5Support()){   
+    var emailCheck = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    
+    if(!emailCheck.test(email.value)){
+        email.setAttribute("class", "notValid");
+        canSubmit = false;
+    }
+    
+    else {
+        email.removeAttribute("class", "notValid"); 
+        email.setAttribute("class", "isValid"); 
+        canSubmit = true;
+    }
+    
+    //}//end of html5support
+}//end of valEmail function
+
+function passwordVal () {
+ //if(!isHTML5Support()){   
+    
+    if(Password1.value !== PasswordCheck.value){
+        Password1.setAttribute("class", "notValid");
+        PasswordCheck.setAttribute("class", "notValid");;
+        canSubmit = false;  
+    }
+    
+    else {
+        Password1.removeAttribute("class", "isValid"); 
+        PasswordCheck.removeAttribute("class", "isValid"); 
+        Password1.setAttribute("class", "isValid");
+        PasswordCheck.setAttribute("class", "isValid");
+        canSubmit = true;
+    }
+
+    //}//end of html5support
+}//end of valEmail function
+
