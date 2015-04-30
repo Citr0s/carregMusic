c8a4fb063925bd93567033a098068769bfb49496
Milosz Dura <milosz.md@wp.pl>
2015-04-30 13:33:24 +0100

    concerts

--- concerts.php
+++ concerts.php
@@ -6,7 +6,7 @@
 ?>
         <div class="container">
             <div class="formContainer">
-                <h2 class="bigH2">Tracks</h2>
+                <h2 class="bigH2">Concerts</h2>
                 <table style="margin:15px;">
                 <?php
                   if(isset($_GET['id'])){
@@ -33,21 +33,19 @@
                       echo '<tr><td><img src="css/coverPictures/'. $trackPicture . '" width="250" /></td><td><p class="artistName">'.$artists.' - '.$trackTitle.'</p></td></tr>';
                     }
                   }else{
-                    $data = mysqli_query($con, "SELECT trackID, tracks.trackTitle, GROUP_CONCAT(artists.artistName SEPARATOR ' & ') 
-                          AS artists, tracks.coverPicture, COUNT(trackArtists.trackID) AS artistCount FROM tracks
-                          INNER JOIN trackArtists USING (trackID)
-                          INNER JOIN artists USING (artistID) 
-                          GROUP BY tracks.trackID
-                          ORDER BY trackTitle ASC");
+                    $data = mysqli_query($con, "SELECT concertID, concerts.concertName, venues.venueName, countries.countryName
+                                                FROM concerts 
+                                                INNER JOIN venues USING (venueID) 
+                                                INNER JOIN countries USING (countryID)
+                                                ORDER BY concertName ASC");   
+  
+                    while($row = mysqli_fetch_array($data)){
+                      $concertID = $row['concertID'];
+                      $concertName = $row['concertName']; 
+                      $venueName = $row['venueName']; 
+                      $countryName = $row['countryName'];
 
-                    while($row = mysqli_fetch_array($data)){ 
-                      $trackTitle = $row['trackTitle'];
-                      $artists = $row['artists']; 
-                      $artistCount = $row['artistCount']; 
-                      $trackPicture = $row['coverPicture'];
-                      $trackID = $row['trackID'];
-
-                      echo '<tr><td><a href="?id='.$trackID.'"><img src="css/coverPictures/'. $trackPicture . '" width="150" /></a></td><td><p class="artistName">'.$artists.' - '.$trackTitle.'</p></td></tr>';
+                      echo '<tr><td><a href="?id='.$concertID.'"></td><td><p class="artistName">'.$concertName.' - '.$venueName.' ('.$countryName.')</p></td></tr>';
                     }
                   }
                 ?>
