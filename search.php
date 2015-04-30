17db535b8bc52bc860bf6048b380806c22b9924b
Milosz Dura <milosz.md@wp.pl>
2015-04-30 15:07:40 +0100

    minor changes

--- search.php
+++ search.php
@@ -8,34 +8,28 @@
             <div class="formContainer">
                 <h2 class="bigH2">Search</h2>
                 <table style="margin:15px;">
+                  <div class="tipP"><p>Search artists, tracks or venues.</p></div>
+                  <form method="get" action=""><input type="text" id="searchLarge" name="search" value="<?php if($_GET){HtmlText($_GET['search']);} ?>" placeholder="album, artist, concert"></form>
                 <?php
-                  if(isset($_GET['id'])){
-                    $id = sanitise(trim($_GET['id']));
-                    if(!is_numeric($id)){
-                      header("Location: search.php");
-                      die();
-                    }
-                      $data = mysqli_query($con, "SELECT tracks.trackTitle, GROUP_CONCAT(artists.artistName separator ' and ') 
+                  if(isset($_GET['search'])){
+                    $search = sanitise(trim($_GET['search']));
+                      $data = mysqli_query($con, "SELECT trackID, tracks.trackTitle, GROUP_CONCAT(artists.artistName separator ' and ') 
                           AS artists, tracks.coverPicture, count(trackArtists.trackID) AS artistCount FROM tracks
                           INNER JOIN trackArtists USING (trackID)
                           INNER JOIN artists USING (artistID) 
-                          WHERE artists.artistName LIKE '%" . $id . "%'
+                          WHERE artists.artistName LIKE '%" . $search . "%'
                           GROUP BY tracks.trackID
-                          ORDER BY artistName ASC");    
+                          ORDER BY artistName ASC");
                       
                       while($row = mysqli_fetch_array($data)){ 
                         $trackTitle = $row['trackTitle']; 
                         $artists = $row['artists']; 
                         $artistCount = $row['artistCount']; 
                         $trackPicture = $row['coverPicture'];
+                        $trackID = $row['trackID'];
 
-                        echo $trackTitle . "<br>"; 
-                        echo $artists . "<br>";
-                        echo $trackPicture . "<br>";
+                        echo '<tr><td><a href="?id='.$trackID.'"><img src="css/coverPictures/'. $trackPicture . '" width="150" /></a></td><td><p class="artistName">'.$artists.' - '.$trackTitle.'</p></td></tr>';
                   }
-                }else{
-                  echo '<div class="tipP"><p>Search artists, tracks or venues.</p></div>';
-                  echo '<tr><td><form method="get" action=""><input type="text" id="searchLarge" name="search" placeholder="album, artist, concert"></form></td></tr>';
                 }
                 ?>
                 </table>
