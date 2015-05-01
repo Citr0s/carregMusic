17044e21348d53e285e188c6c3fdd4ffb4ce6c8b
rogerogerson <addk92@gmail.com>
2015-05-01 23:09:27 +0100

    footer images added

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,15 @@
         <div id="footer">
             <div class="container">
                <div class="floatLeft">
-                    <a href="index.php"><img src="css/images/logoBottom.png" alt="logo"></a>
+                    <a href="index.php" id="footerLogo"><img src="css/images/logoBottom.png" alt="logo"></a>
                 </div>
                 <div class="floatLeft">
                     <p>&copy; CARREG MUSIC 2015</p>
+                </div>
+                <div id="footerImgContainer">
+                    <a href="#"><img src="images/facebook.png" class="socialImg" alt="facebook link"></a>
+                    <a href="#"><img src="images/twitter.png" class="socialImg" alt="twitter link"></a>
+                    <a href="#"><img src="images/instagram.png" class="socialImg" alt="instagram link"></a>
                 </div>
             </div>
         </div>
