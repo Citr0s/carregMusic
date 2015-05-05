8ccd722b6d65c1fc7a4a438bfdebd0372761a297
Milosz Dura <milosz.md@wp.pl>
2015-05-05 17:37:24 +0100

    added artists details to concert page

--- concerts.php
+++ concerts.php
@@ -25,44 +25,66 @@
 
                     echo '<a class="backLink" href="concerts.php">< Back</a>';
 
+                    $row = mysqli_fetch_array($data);
+                    $concertID = $row['concertID'];
+                    $concertName = $row['concertName']; 
+                    $venueName = $row['venueName'];
+                    $venueLongitude = $row['venueLongitude'];
+                    $venueLatitude = $row['venueLatitude'];
+                    $venueCapacity = $row['venueCapacity'];
+                    $countryName = $row['countryName'];
+
+                    ?>
+                    <script>
+                      var myCenter = new google.maps.LatLng(<?php echo $venueLatitude; ?>,<?php echo $venueLongitude; ?>);
+
+                      function initialize()
+                      {
+                      var mapProp = {
+                        center:myCenter,
+                        zoom:15,
+                        mapTypeId:google.maps.MapTypeId.ROADMAP
+                        };
+
+                      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+                      var marker = new google.maps.Marker({
+                        position:myCenter,
+                        });
+
+                      marker.setMap(map);
+                      }
+
+                      google.maps.event.addDomListener(window, 'load', initialize);
+                    </script>
+                    <?php
+
+                    echo '<tr><td><p class="artistName">'.$concertName.' - '.$venueName.' ('.$countryName.')</p></td></tr>';
+                    echo '<tr><td><p class="artistName">Capacity: '.$venueCapacity.'</p></td></tr>';
+                    echo '<tr><td><div id="googleMap" style="width:500px;height:380px;"></div></td></tr>';
+
+                    $count = 0;
+                    $data = mysqli_query($con, "SELECT * FROM artists INNER JOIN concertartists USING (artistID)
+                                                INNER JOIN concerts USING (concertID)
+                                                WHERE concertName LIKE '%" . $concertName . "%' 
+                                                ORDER By artistName ASC");    
+
+                    echo '</table><table style="margin:15px;"><tr><td><p>Artists apearing at this concert: </p></td></tr>';
+                    
                     while($row = mysqli_fetch_array($data)){ 
-                      $concertID = $row['concertID'];
-                      $concertName = $row['concertName']; 
-                      $venueName = $row['venueName'];
-                      $venueLongitude = $row['venueLongitude'];
-                      $venueLatitude = $row['venueLatitude'];
-                      $venueCapacity = $row['venueCapacity'];
-                      $countryName = $row['countryName'];
+                      $artistName = $row['artistName'];
+                      $artistHistory = $row['artistHistory'];
+                      $artistPicture = $row['artistPicture'];
+                      $artistID = $row['artistID'];
+                      $count++;
 
-                      ?>
-                      <script>
-                        var myCenter = new google.maps.LatLng(<?php echo $venueLatitude; ?>,<?php echo $venueLongitude; ?>);
-
-                        function initialize()
-                        {
-                        var mapProp = {
-                          center:myCenter,
-                          zoom:15,
-                          mapTypeId:google.maps.MapTypeId.ROADMAP
-                          };
-
-                        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-                        var marker=new google.maps.Marker({
-                          position:myCenter,
-                          });
-
-                        marker.setMap(map);
-                        }
-
-                        google.maps.event.addDomListener(window, 'load', initialize);
-                      </script>
-                      <?php
-
-                      echo '<tr><td><p class="artistName">'.$concertName.' - '.$venueName.' ('.$countryName.')</p></td></tr>';
-                      echo '<tr><td><p class="artistName">Capacity: '.$venueCapacity.'</p></td></tr>';
-                      echo '<tr><td><div id="googleMap" style="width:500px;height:380px;"></div></td></tr>';
+                      echo '<tr><td><a href="artists.php?id='.$artistID.'"><img src="css/artistPictures/'. $artistPicture . '" width="250" /></a></td><td><p><a href="artists.php?id='.$artistID.'">'.$artistName.'</a></p></td></tr>';
                     }
+
+                    if($count === 0){
+                      echo '<tr><td><p class="usernameC">None</p></td></tr>';
+                    }
+
                   }else{
                     $data = mysqli_query($con, "SELECT concertID, concerts.concertName, venues.venueName, countries.countryName
                                                 FROM concerts 
